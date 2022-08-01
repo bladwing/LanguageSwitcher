e91cbdc33abe813a0aaafbed74b600e0e28d0015
BLADWING <timoti.ponarenkO@gmail.com>
2022-08-02 01:45:36 +0400

    correct footer style add effects...

--- src/components/Module/Footer.jsx
+++ src/components/Module/Footer.jsx
@@ -6,16 +6,22 @@ export default function Footer() {
 
   return (
     <footer>
-      <span className="Year">{new Date().getFullYear()} {t("footer.year")}</span> <br />{" "}
-      {t("footer.nameOfCreator")}  <br />
+      
+     <div className="portfolio"><a href={t("footer.portfolioLink")} target="_blank" rel="noopener noreferrer" className="portfolio"> <h2 className="flipX">{t("footer.nameOfCreator")}</h2></a> </div>  <br />
       {t("footer.infoSource")}{" "}
       <a
         href={t("footer.SourceLink")}
         target="_blank"
         rel="noopener noreferrer"
-      >
+        className="source"
+      > {" "}
         {t("footer.Source")}
       </a>
+      <br />
+      <span className="Year">&copy; {new Date().getFullYear()} {t("footer.year")}</span> <br />{" "}
+     
+
+ 
     </footer>
   );
 }
